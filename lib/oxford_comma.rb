6612f63129254f13cb85784c91135dc5d54abcb3
Nicholas Patel <np4795@yahoo.com>
2020-02-12 23:57:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
 
+  if array.length == 1 
+    return array[0]
+  elseif array.length == 2 
+    return array[0] + " and " + array[1]
+  else array.length > 2 
+  a = []
+  b = []
+  str = " "
+  count = 0 
+  while count < array.length - 1 
+  a << array[count]  + ", "
+  count = count + 1 
+end
+a.join
+a.push("and " + array[-1])
 end
