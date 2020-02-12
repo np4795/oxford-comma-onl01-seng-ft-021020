b43a06a0bf213aa2046663f526157ca324072c0b
Nicholas Patel <np4795@yahoo.com>
2020-02-12 23:58:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
 
   if array.length == 1
-end 	    return array[0]
+	    return array[0]
   elsif array.length == 2
     return array[0] + " and " + array[1]
   elsif array.length > 2
